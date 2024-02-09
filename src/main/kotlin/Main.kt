418f80c7b6a84c2cf1ad5a0c10e1842a81d78c2a
Scarlet-Alkeyo <scarletalkeyo@gmail.com>
2024-02-09 07:18:34 +0300

    added data

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var fullname= "Jane Wangui"
+    println (fullname)
+    var age=20
+    println (age)
+    var phonenumber= "0756784567"
+    println (phonenumber)
+    var weight=56
+    println (weight)
+    var Nationality="Kenyan"
+    println (Nationality)
+
+
+
+
+
+}
